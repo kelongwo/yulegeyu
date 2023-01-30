a2cb19b02e3bb285790f6c513ff01edac795c5e1
kelongwo <120772251+kelongwo@users.noreply.github.com>
2023-01-30 17:58:12 +0800

    Add files via upload

---
+++ src/components/MyAd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="my-ad">
+    <a href="https://github.com/kelongwo/yulegeyu" target="_blank">
+      <div style="background: rgba(0, 0, 0, 0.8); padding: 12px">
+        <github-outlined />
+        代码完全开源 欢迎下载
+      </div>
+    </a>
+    <!-- <a href="https://blog.csdn.net/m0_68539124"> 欢迎关注克隆窝 </a>-->
+  </div>
+</template>
+<script setup lang="ts">
+import { GithubOutlined } from "@ant-design/icons-vue";
+</script>
+<style></style>
